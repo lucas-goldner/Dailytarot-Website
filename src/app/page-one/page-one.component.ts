927d9cb30586ac6dd30b4bb6f8a77dc356c5f564
Lucas Goldner <lucas.goldner@googlemail.com>
2020-11-23 00:11:09 +0100

    Detect if mobile device

--- src/app/page-one/page-one.component.ts
+++ src/app/page-one/page-one.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HostListener } from '@angular/core';
 
 export interface Tile {
   cols: number;
@@ -17,6 +18,10 @@ export interface Tile2 {
   styleUrls: ['./page-one.component.css'],
 })
 export class PageOneComponent implements OnInit {
+  mobHeight: any;
+  mobWidth: any;
+  isTouch: any;
+
   tiles: Tile[] = [
     { text: 'DailyTarot', cols: 3 },
     { text: 'About', cols: 1 },
@@ -26,11 +31,23 @@ export class PageOneComponent implements OnInit {
   ];
 
   tiles2: Tile2[] = [
-    { text: 'One', cols: 1, rows: 5 },
-    { text: 'Two', cols: 1, rows: 5 },
+    { text: 'One', cols: 1, rows: 1 },
+    { text: 'Two', cols: 1, rows: 1 },
   ];
+  constructor() {
+    this.mobHeight = window.screen.height - 100 + 'px';
+    this.mobWidth = window.screen.width + 'px';
+    console.log(this.mobHeight);
+  }
 
-  constructor() {}
+  isTouchDevice() {
+    var el = document.createElement('div');
+    el.setAttribute('ontouchstart', 'return;');
+    this.isTouch = typeof el.ontouchstart === 'function';
+    console.log(this.isTouch);
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.isTouchDevice();
+  }
 }
