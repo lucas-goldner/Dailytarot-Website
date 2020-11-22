2e5923f78a7a6da16a1d8c59318cbd889ea61112
Lucas Goldner <lucas.goldner@googlemail.com>
2020-11-22 18:33:24 +0100

    First Material component Test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MatSliderModule } from '@angular/material/slider';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      initialNavigation: 'enabled',
+    }),
+  ],
+  exports: [RouterModule, MatSliderModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
